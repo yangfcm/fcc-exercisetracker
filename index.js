7e1f39dbe6c3d2b7317a49c71edec2bdb8a2d6af
yangfcm <yangfcm@outlook.com>
2023-08-09 09:12:59 +0930

    Connect MongoDB.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express();
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database is connected.");
+  });
+
+const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
