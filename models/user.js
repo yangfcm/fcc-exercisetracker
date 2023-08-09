035c5b59cd4c8b20df8ab36131846c8dff999be9
yangfcm <yangfcm@outlook.com>
2023-08-09 16:23:29 +0930

    Add User model.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
 const User = mongoose.model("User", UserSchema);
 
